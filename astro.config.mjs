ae7470e0d54d2d211598640891f007ee4e81d5c4
chemikal <josemaf.flores@gmail.com>
2025-12-26 17:41:59 +0100

    Configuración final de dominio y activación de sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // ¡AÑADE ESTA LÍNEA AQUÍ ABAJO!
+  site: 'https://www.softwareoficios.es', 
+  
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap()]
 });
